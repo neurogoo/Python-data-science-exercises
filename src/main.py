66b91313b3420afd1c465c13dfe8bb07c03e20c5
Toni Okuogume <tokuogume@gmail.com>
2016-11-12 13:50:21 +0200

    Solution for week2 exercises

--- src/main.py
+++ src/main.py
@@ -22,7 +22,13 @@ usage_file_test = "../data/small/some-lupapiste-usage-pub-20161031.csv"
 usage_file = "../data/all-lupapiste-usage-pub-20161031.csv"
 operative_file = "../data/all-applications-operative-pub-20161031.csv"
 df = read_data(usage_file)
-df_operative = pd.read_csv(operative_file, sep = ';')
+df_operative = pd.read_csv(operative_file, parse_dates=['createdDate', 'submittedDate'], sep = ';')
 print('Amount of municipalites that have used the service is {}').format(df.municipalityId.unique().size)
 print("Amount of application role users is {}").format(df[df['role'] == 'applicant'].userId.unique().size)
 print("Amount of authority role users is {}").format(df[df['role'] == 'authority'].userId.unique().size)
+print('Amount of comments on each application is:')
+print(df[(df['action'] == 'add-comment')].groupby('applicationId').count().userId)
+#Create month column from createDate
+df_operative['createdMonth'] = df_operative['createdDate'].map(lambda x: x.month)
+createdMonthPlot = df_operative[df_operative['state'] == 'submitted'].groupby('createdMonth').size().plot(kind='bar', title='Amount of application submitted each month')
+createdMonthPlot.set_xlabel('Month')
